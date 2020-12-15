7384208dee036b8f1567aa035e0b6d6d060a2505
Pablo <pablo.velibarquin@alum.uca.es>
2020-12-15 18:19:31 +0100

    Reset Password

--- register.php
+++ register.php
@@ -155,8 +155,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
    <!-- Custom CSS -->
    <link rel="stylesheet" href="css/custom.css">
    <!-- font family -->
-   <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    
+   <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">    
 </head>
 <body>
     <div class="register-box">
