045be943905c4cddf8d65004718db04b12c252e6
Pablo <pablo.velibarquin@alum.uca.es>
2021-01-08 17:20:07 +0100

    Ranking Html

--- includesranking/components/getRanking/content.php
+++ includesranking/components/getRanking/content.php
@@ -3,24 +3,21 @@ include_once 'includesranking/config/functions.php';
 $qRes = "SELECT * FROM users WHERE pinfcoins >= 0 ORDER BY pinfcoins DESC LIMIT 0, 100";
 $qQuery = $mysqli->query($qRes);    
 $i = 0;
+$cont = 0;
 echo '<ul class="list-group list-group-flush">';
 if ($qQuery->num_rows > 0){
-while($qRow = $qQuery->fetch_assoc()){ 
+while($qRow = $qQuery->fetch_assoc() AND $cont < 10){ //Sirve para mostrar un top 10 en Ranking
 //$qReplace = $qRow['pinfcoins']; //shows the pinfcoins as follows: 234252324
 $qReplace = number_format($qRow['pinfcoins'], 0, '.', '.'); //shows the pinfcoins as follows: 234.252.324
 $i++;
 ?>
-<li class="list-group-item d-flex justify-content-between align-items-center">
-	<?php if($i == 1 OR $i == 2 OR $i == 3){ ?>
-		<span class="pull-left"><i class="fas fa-trophy" style="color: <?php echo colorRank($i); ?>;"></i> &nbsp; <?php echo $qRow['name'];echo ", '";echo $qRow['username'];echo "'"; ?></span>
-	<?php }else{ ?>
-		<span class="pull-left"> &nbsp; <b><?php echo $i; ?></b> &nbsp; <?php echo $qRow['name'];echo ", '";echo $qRow['username'];echo "'"; ?></span>
-	<?php } ?>
-    <span class="badge badge-primary badge-pill">
-		<span class="badge badge-primary badge-pill"><?php echo levelRank($qRow['pinfcoins']); ?></span>
+<li>
+	<mark><?php echo $qRow['name'];echo ", '";echo $qRow['username'];echo "'"; $cont++; ?></mark>
+    <small class="badge badge-primary badge-pill">
+		<span><?php echo levelRank($qRow['pinfcoins']); ?></span>
 		&mdash;
-		<?php echo $qReplace; ?> <small>PinfCoins</small>
-	</span>
+		<?php echo $qReplace; ?> PinfCoins
+	</small>
 </li>
 <?php 
 }
@@ -29,4 +26,3 @@ $i++;
 	echo '<ul class="list-group"><li class="list-group-item text-center"><span>Nenhum <b>usuário</b> foi classificado ainda.</span></li></ul>';
 }
 ?>
-<!-- style="background-color: #e85a5b;border: 1px solid #e85a5b;" -->
--- rankingmain.php
+++ rankingmain.php
@@ -1,5 +1,5 @@
 <?php
-//Chama os arquivos necessario
+//Archivos necesarios
 include_once __DIR__ . '/includesranking/config/config.php';
 include_once __DIR__ . '/includesranking/config/init.php';
 
@@ -8,37 +8,49 @@ $imgbinary = fread(fopen($img_src, "r"), filesize($img_src));
 $img_str = base64_encode($imgbinary);
 ?>
 <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <link rel="icon" type="image/x-icon" href="images/MonedaFinal-ConvertImage.ico" />
-      <meta charset="utf-8">
-      <title>5&Bet Ranking</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="icon" href="favicon.ico" type="image/x-icon" />
-      <link rel="stylesheet" href="assetsranking/css/bootstrap.min.css">
-      <link rel="stylesheet" href="assetsranking/css/font-awesome.css">
-      <link rel="stylesheet" href="assetsranking/css/style.css?v=<?php echo time(); ?>">
-   </head>
-   <body>
-      <div class="container" style="margin: 30px auto;">
-         <div class="row justify-content-center align-items-center">
-            <div class="col-md-12">
-               <div class="card">
-                  <img class="card-img-top" src="data:image/jpg;base64,<?php echo $img_str; ?>">
-                  <div class="card-body">
-                     <h5 class="card-title"></h5>
-                     <p class="card-text"></p>
-                  </div>
-                  <?php require __DIR__ . '/includesranking/components/getRanking/loader.php'; ?>
-                  <div class="card-body text-center">
-                  <a href="main.php" class="btn btn">Volver</a>
-                  </div>
-               </div>
-            </div>
-         </div>
+<html lang="es">
+
+<head>
+
+   <script src = "js/jquery-3.5.1.js"></script>
+   <script>
+
+         $(document).ready(function() {
+
+            $("body").hide().fadeIn(2000);
+
+         });
+
+   </script>
+
+   <link rel="icon" type="image/x-icon" href="images/MonedaFinal-ConvertImage.ico" />
+   <meta charset="utf-8">
+   <title>5&Bet Ranking</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="icon" href="favicon.ico" type="image/x-icon" />
+   <link rel="stylesheet" href="css/reset.css">
+   <link rel="stylesheet" href="css/ranking.css">
+   <link rel="stylesheet" href="assetsranking/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assetsranking/css/font-awesome.css">
+   <link rel="stylesheet" href="assetsranking/css/style.css?v=<?php echo time(); ?>">
+</head>
+
+<body>
+   <div class="leaderboard">
+      <a href="main.php"><img src="images/logo.png" class="avatar" alt="Imagen Avatar"></a> 
+      <h1>
+         <img class="moneda" src="images/MonedaFinal.PNG">
+         Top 10 Jugadores
+         <img class="moneda" src="images/MonedaFinal.PNG">
+      </h1>
+      <ol>
+         <?php require __DIR__ . '/includesranking/components/getRanking/loader.php'; ?>
+      </ol>
+      <div class="volver">
+         <a href="main.php" class="btn">Volver</a>
       </div>
-      <script type="text/javascript" src="assetsranking/js/jquery.min.js"></script>
-      <script type="text/javascript" src="assetsranking/js/bootstrap.min.js"></script>
-      <script type="text/javascript" src="assetsranking/js/pace.min.js"></script>
-   </body>
+   </div>
+
+</body>
+
 </html>
