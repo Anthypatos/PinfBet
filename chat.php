743fefb440405165eb1df39d5677e9e9e904992d
Pablo <pablo.velibarquin@alum.uca.es>
2021-01-10 17:50:58 +0100

    Chat

--- chat.php
+++ chat.php
@@ -25,76 +25,84 @@
     else
     {
 ?>
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>5&Bet - Chat</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-            <script src = "js/jquery-3.5.1.js"></script>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<link rel="icon" type="image/x-icon" href="images/MonedaFinal-ConvertImage.ico" />  
+    <title>5&Bet - Chat</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/all.css" > <!-- Iconos de FontAwesome -->
+    <link rel="stylesheet" href="css/principal.css">
+    <link rel="stylesheet" href="css/social.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src = "js/jquery-3.5.1.js"></script>
 
-            <script type = "text/javascript">
+    <script type = "text/javascript">
 
-                $(document).ready(function() {
+        $(document).ready(function() {
 
-                    setInterval (cargar_log, 500);  // Recarga el registro del chat cada 500ms
+            setInterval (cargar_log, 500);  // Recarga el registro del chat cada 500ms
 
-                    // Solicita el registro del chat
-                    function cargar_log()
-                    {
-                        var datos = {user_actual:<?php echo $user_actual; ?>,   // Se envían las IDs del cliente
-                                    user_otro:<?php echo $user_otro; ?>}        // y el usuario con el que se chatea
+            // Solicita el registro del chat
+            function cargar_log()
+            {
+                var datos = {user_actual:<?php echo $user_actual; ?>,   // Se envían las IDs del cliente
+                    user_otro:<?php echo $user_otro; ?>}        // y el usuario con el que se chatea
 
-                        $.get("procesarchat.php", datos, mostrar_chat); // Solicita los datos
-                    }
+                $.get("procesarchat.php", datos, mostrar_chat); // Solicita los datos
+            }
 
-                    // Muestra la conversación en el contenedor
-                    function mostrar_chat(datos_rec)
-                    {
-                        $("#caja_chat").html(datos_rec);
-                    }
+            // Muestra la conversación en el contenedor
+            function mostrar_chat(datos_rec)
+            {
+                $("#caja_chat").html(datos_rec);
+            }
 
-                    // Envía los mensajes del usuario cliente al servidor
-                    $("#formulario").submit(function(event) {
+            // Envía los mensajes del usuario cliente al servidor
+            $("#formulario").submit(function(event) {
 
-                        event.preventDefault(); // Cancela el comportamiento predeterminado del formulario
+                event.preventDefault(); // Cancela el comportamiento predeterminado del formulario
 
-                        var datos_env = $(this).serialize();    // Envuelve los datos del formulario
+                var datos_env = $(this).serialize();    // Envuelve los datos del formulario
 
-                        $.post("procesarchat.php", datos_env);  // Envía los datos
+                $.post("procesarchat.php", datos_env);  // Envía los datos
 
-                        $("#mensaje").val("");  // Limpia el campo texto del formulario
+                $("#mensaje").val("");  // Limpia el campo texto del formulario
 
-                    });
-                })
-            </script>
-
-        </head>
-        <body style = "text-align:center">
+            });
+        })
+    </script>
+</head>
+<body>
                 
-            <!-- Barra de navegación -->
-            <?php include "barra_navegacion.php"; ?>
-
-            <h1 style = "margin-top:75px;">Chat</h1>
+    <!-- Barra de navegación -->
+    <?php include "barra_navegacion.php"; ?>
+    <div class="w3-container w3-content" style="margin-top:80px"> 
+        <h1 style = "margin-top:75px;">Chat</h1>
 <?php
-            $username_otro = mysqli_fetch_array(mysqli_query($link, "SELECT username FROM users WHERE id = '$user_otro'"))['username'];
-            echo "Chateando con <i>" . $username_otro . "</i>";
+        $username_otro = mysqli_fetch_array(mysqli_query($link, "SELECT username FROM users WHERE id = '$user_otro'"))['username'];
+        echo "Chateando con <i>" . $username_otro . "</i>";
 ?>
-            <!-- Contenedor de conversación -->
-            <div class = "container" id = "caja_chat" style = "border:1px solid black; padding:20px; margin-top:10px;"></div>
-            <br>
+        <!-- Contenedor de conversación -->
+        <div class = "container" id = "caja_chat" style = "border:1px solid black; padding:20px; margin-top:10px;"></div>
+        <br>
 
-            <!-- Formulario de envío -->
-            <form id = "formulario" method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?id=" . $user_otro; ?>">
-                <input type = "text" id = "mensaje" name = "mensaje" placeholder = "Escribe un mensaje..." value = "">
-                <input type = "hidden" id = "user_actual" name = "user_actual" value = "<?php echo $user_actual; ?>">
-                <input type = "hidden" id = "user_otro" name = "user_otro" value = "<?php echo $user_otro; ?>">
-                <input type = "submit" value = "Enviar">
-            </form>
-        </body>
-        </html>
+        <!-- Formulario de envío -->
+        <form id = "formulario" method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) . "?id=" . $user_otro; ?>">
+            <input type = "text" id = "mensaje" name = "mensaje" placeholder = "Escribe un mensaje..." value = "">
+            <input type = "hidden" id = "user_actual" name = "user_actual" value = "<?php echo $user_actual; ?>">
+            <input type = "hidden" id = "user_otro" name = "user_otro" value = "<?php echo $user_otro; ?>">
+            <input type = "submit" value = "Enviar">
+        </form>
+    </div>
+</body>
+</html>
 
 <?php
     }
 ?>
+
