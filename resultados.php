7fb603bb067627a8454c45e93fe673466cfa7f5e
RaulArcos <1.raularcos@gmail.com>
2021-01-11 17:10:58 +0100

    sadfsda

--- resultados.php
+++ resultados.php
@@ -3,7 +3,7 @@
 session_start();
  
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false && $_SESSION["admin"] != 1){
     header("location: login.php");
     exit;
 }
