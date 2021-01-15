c19934a32ab6fca0e7aaabe542720f3d548941d6
RaulArcos <1.raularcos@gmail.com>
2021-01-15 19:51:00 +0100

    asdsad

--- main.php
+++ main.php
@@ -2,6 +2,7 @@
 // Initialize the session
 session_start();
 header('Content-Type: text/html; charset=UTF-8');
+header('Content-Type: text/html; charset=ISO-8859-1');
 // Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
